1295bf56577da0f892e77540ab2e76825d1e1971
manasrda <manasrda@mail.uc.edu>
2020-08-14 21:03:31 -0400

    added main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "East US"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name                  = "weatherapi"
+    location              = azurerm_resource_group.tf_test.location
+    resource_group_name   = azurerm_resource_group.tf_test.name
+
+    ip_address_type     = "public"
+    dns_name_label      = "manasrdawa"
+    os_type             = "Linux"
+
+    container {
+        name        = "weatherapi"
+        image       = "manasrda/weatherapi"
+            cpu         = "1"
+            memory      = "1"
+
+            ports {
+                port        = 80
+                protocol    = "TCP"
+            }
+
+    }
+}
